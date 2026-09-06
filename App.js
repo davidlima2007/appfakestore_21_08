d1051429bfb7066dfab8e1f78c05a7ff6ad743b2
davidlima2007 <daviddam7401@gmail.com>
2026-09-06 02:35:47 +0000

    Atividade_Rony

--- App.js
+++ App.js
@@ -1,65 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-  export default function App() {
+export default function App() {
 
   const [dados, setDados] = useState([])
 
+
   async function carregaProdutos() {
     try {
-    let resposta= await fetch("https://fakestoreapi.com/products/");
-    if (resposta.status == 200) {
-      let novosDados = await resposta.json();
-      setDados(novosDados);
-    }
-    else {
-      throw Exception("Falha no carregamento de dados");
+      let resposta = await fetch("https://fakestoreapi.com/products/")
+      if (resposta.status == 200) {
+        let novosDados = await resposta.json();
+        setDados(novosDados);
+      }
+      else {
+        throw Exception("falha de carregamento de dados");
       }
     }
     catch (e) {
       console.log(e)
       throw Exception("Falha no carregamento de dados")
     }
-   }
-
-    useEffect(() => {
-    carregaProdutos()
-    }, []);
-
-    return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-
-      <FlatList
-      style={{ width: '100%' }}
-      data={dados}
-      keyExtractor={(item)=>item.id.tostring()}
-      renderItem={({ item })=>(
-      <View style={styles.card}>
-      <Image source={{ uri: item.image }} style={{ width: 50, height: 50}} />
-      <Text>{item.title}</Text>
-      <Text>{item.price}</Text>
-      </View>
-      
-      )}
-      />
-      <StatusBar style="auto" />
-      </View>
-    );
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    card: {
-      flexDirection: 'column',
-      elevation:8,
-      padding: 16,
-      width:'100%'
-    }
-  });
+  useEffect(()=>{
+    carregaProdutos()
+  }, []);
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+      style={{width: '100%', flex:1}}
+      data={dados}
+      keyExtractor={(item)=>item.id.toString()}
+      renderItem={({item})=>(
+        <View style={styles.card}>
+          <Image source={{uri: item.image}} style={{width:50, height:50}} />
+          <Text>{item.title}</Text>
+          <Text>{item.price}</Text>
+
+        </View>
+  )}
+      ></FlatList>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card:{
+    flexDirection: 'column',
+    color: '#FFFF',
+    elevation: 8,
+    padding: 16,
+    width: '100%'
+  },
+  
+});
